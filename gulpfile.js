96a4bea154e221aaf43a601b573301500a2febfc
shaqdeff <shaquillendunda@gmail.com>
2022-04-17 06:32:19 +0300

    Create dieted folder and add variables.scss and base.scss files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
- return src('styles.scss')
+ return src('dieted/**/*.scss')
   .pipe(sass())
   .pipe(dest('css'))
 }
 
 function watchTask() {
- watch(['styles.scss'], buildStyles)
+ watch(['dieted/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
